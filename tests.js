ce62289144c140e3fe0aed702e58197a15486568
richhaigh <richard@richhaigh.co.uk>
2016-03-08 13:47:17 +0000

    initial commit

---
+++ tests.js
@@ -0,0 +1,32 @@
+var objecttype = require('./index');
+
+var obj = {value: 'Hello world'};
+var str = 'Hello world';
+var num = 999;
+var date = new Date();
+var func = function(){
+	console.log('Hello world');
+}
+var bool = true;
+var arr = [1,2,3];
+var regex = /test/;
+
+console.log(objecttype.getType(obj));
+
+console.log(objecttype.getType(str));
+
+console.log(objecttype.getType(num));
+
+console.log(objecttype.getType(date));
+
+console.log(objecttype.getType(func));
+
+console.log(objecttype.getType(bool));
+
+console.log(objecttype.getType(arr));
+
+console.log(objecttype.getType(regex));
+
+if(objecttype.getType(str) == objecttype.string){
+	console.log("It's a string!");
+}
